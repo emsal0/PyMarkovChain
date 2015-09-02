db36a53cd203d08364b9fba7da7f82f1b1854e52
Em Sal <absolutezero2a03@gmail.com>
2015-09-01 20:50:55 -0700

    errors fixed

--- textmarkov.py
+++ textmarkov.py
@@ -1,5 +1,6 @@
 import markov
 import pickle
+import time
 
 def get_bare(word):
     puncts = [',','.']
@@ -14,20 +15,23 @@ wordlist = filter(lambda a: a != "", wordlist)
 
 anna_corpus = markov.Corpus()
 i=0
-while i < 20000:
+time.sleep(1)
+while i < len(wordlist) - 1:
     curr_word = wordlist[i]
     next_word = wordlist[i+1]
+    i += 1
     if curr_word[0] in ["\""]:
         t = curr_word[0]
         curr_word = curr_word[1:]
-        anna_corpus.add_one((t,curr_word))
+        if len(curr_word) == 0:
+            continue
+        anna_corpus.add_one((t,get_bare(curr_word)))
     if curr_word[-1] == ',' or curr_word[-1] == '.':
         anna_corpus.add_one((curr_word[:-1],curr_word[-1]))
         curr_word = curr_word[-1]
     if next_word[0] in ["\""]:
         next_word=next_word[0]
     anna_corpus.add_one((curr_word,get_bare(next_word)))
-    i += 1
 
 with open("anna_corpus.pickle.txt","w") as f:
     pickle.dump(anna_corpus,f)
